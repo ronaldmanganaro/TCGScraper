1280c944a2d08043d8dc7471fbfdc3fbbed9feae
Ronald Manganaro <RonaldManganaro@gmail.com>
2025-03-17 16:54:57 -0400

    added var for volumes

--- docker/initDB.py
+++ docker/initDB.py
@@ -1,27 +1,49 @@
 import psycopg2
-import logging 
+import logging
+import sys
+
+import psycopg2.errorcodes
 
 def createTable():
-    dbname = 'TCGPlayerDB'
+    myDb = 'TCGPlayerDB'
     user = 'rmangana'
     password = 'password'
     host = 'localhost'
     port = 5432
 
-   
     try:
-        newConnection =  psycopg2.connect(
-            dbname=dbname,
+        initConnection =  psycopg2.connect(
+            dbname="postgres",
             user=user,
             password=password, 
             host=host,
             port=port
         )
+        initConnection.autocommit = True
+        cursor = initConnection.cursor()
+
+        cursor.execute(f"SELECT 1 FROM pg_database WHERE datname = 'postgres';")
+        exists = cursor.fetchone
+
+        if not exists:
+            logging.info(f"The {myDb} does not exist executing query")
+            cursor.execute(f"CRAETE DATABASE '{myDb}';")
+        else:
+            logging.info(f"The {myDb} exists skipping query")
+        
+        newConnection =  psycopg2.connect(
+                dbname=myDb,
+                user=user,
+                password=password, 
+                host=host,
+                port=port
+            )
         cursor = newConnection.cursor()
+
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS TCGPlayerNew (
                 Card VARCHAR(100),
-                Listing_Quantity INT,
+                Listing_Quantity integer,
                 Lowest_Price MONEY,
                 Market_Price MONEY,
                 Rarity VARCHAR(100),
@@ -34,9 +56,17 @@ def createTable():
         cursor.close()
         newConnection.close()
         
-    except Exception as e:
-        print("ad")
+    except psycopg2.OperationalError as e:
+        logging.info(f"Connection error: {e.pgcode}")
 
 if __name__ == "__main__":
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.StreamHandler(sys.stdout)  # Send logs to stdout
+        ]
+    )
+
     createTable()
-    
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 
 # Start runner
 C:\Users\MrCool\Documents\Repos\actions-runner\run.cmd
+
+# Set the env
+[System.Environment]::SetEnvironmentVariable("VOLUME_PATH", "C:\Users\WinMax2\Documents\Repos\postgres\data")
+docker-compose up -d
