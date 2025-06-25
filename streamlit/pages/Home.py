e0bf55223a478488dcd31aa0de381b6f72943406
Ronald Manganaro <RonaldManganaro@gmail.com>
2025-06-25 16:42:18 -0400

    using db for users and certain pages are only accessible to my account

--- streamlit/pages/Home.py
+++ streamlit/pages/Home.py
@@ -6,10 +6,9 @@ from streamlit_extras.buy_me_a_coffee import button as coffee_button
 from functions import widgets
 # Set page configuration globally at the start
 
-st.title("🏠 Home")
+st.title("🏠 Home", anchor=None, help=None, width="stretch")
 
 # ---- Sidebar Navigation ----
-widgets.show_pages_sidebar()
 st.sidebar.header("Choose a TCG", divider=True)
 
 # Section for selecting TCG
@@ -74,6 +73,8 @@ else:
     st.info("We’re planning to add tools for Yu-Gi-Oh!, Flesh and Blood, and more.")
     st.image("https://cdn.vox-cdn.com/thumbor/dTQveLbtHn9ZJhH7qZoTx2Ika4A=/0x0:1300x650/1400x933/filters:focal(546x226:754x434):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/72780383/Screen_Shot_2024_01_24_at_9.23.56_AM.0.png",
              caption="Future support for more TCGs", use_column_width=True)
+
+widgets.show_pages_sidebar()
 
 # ---- Footer ----
 st.markdown("---")  # Add a horizontal line to separate the footer
