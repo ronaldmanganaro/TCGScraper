9607fbb8b4977a76de8931b59bdbd2c20a248bb4
Ronald Manganaro <ronaldmanganaro@gmail.com>
2025-08-09 16:41:54 -0400

    terraform

---
+++ terraform/envs/dev/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "The type of AWS instance to create"
+  type        = string
+  default     = "t2.micro"
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,25 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
+}
+
+resource "aws_instance" "test" {
+  instance_type =var.instance_type
+  ami = "ami-084568db4383264d4" # Example AMI, replace with a valid one  
 }
 
 resource "aws_s3_bucket" "example" {
   bucket = "terraform-streamlit-bucket-test-1128731"
+}
+
+data "aws_instances" "example" {
+  instance_tags = {
+    Environment = "Production"
+  }
+
+  instance_state_names = ["running", "stopped"]
+}
+
+resource "aws_eip" "test" {
+  count = length(data.aws_instances.example.ids)
+  instance = data.aws_instances.example.ids[count.index]
 }
